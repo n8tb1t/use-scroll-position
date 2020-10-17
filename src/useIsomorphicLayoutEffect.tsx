b5c1538331ec765e299ed8a7e506147a0f98844f
zach <n8tb1t@gmail.com>
2020-10-17 14:33:56 +0300

    fix(refactoring): Replace tabs

--- src/useIsomorphicLayoutEffect.tsx
+++ src/useIsomorphicLayoutEffect.tsx
@@ -1,4 +1,4 @@
 import { useLayoutEffect, useEffect } from 'react';
 
 export const useIsomorphicLayoutEffect =
-	typeof window !== 'undefined' ? useLayoutEffect : useEffect;
+  typeof window !== 'undefined' ? useLayoutEffect : useEffect;
