a8748727fe028c64fb5104ecf8cf1d44a8c8f179
n8tb1t <n8tb1t@gmail.com>
2025-09-11 02:32:35 +0300

    feat(cicd): update gh-pages script

--- packages/docs/react-router.config.ts
+++ packages/docs/react-router.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from '@react-router/dev/config'
 
 export default {
+  basename: '/use-scroll-position/',
   buildDirectory: '.build',
   ssr: false,
   prerender: async () => ['/', 'sidebar', 'position']
